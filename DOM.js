e4377f7282cb57ce0fa633f5d39fcdae3a47c3de
SHRIJADHANALAKSHMI <shrijasm@gmail.com>
2026-05-26 12:49:13 +0530

    dom basic added

---
+++ DOM.js
@@ -0,0 +1,29 @@
+//change text
+let textbtn=document.getElementById("textBtn");
+textbtn.addEventListener("click",()=>{
+    let title=document.getElementById("title");
+    title.textContent="Text changed done : hello!";
+});
+let colorbtn=document.getElementById("colorBtn");
+colorbtn.addEventListener("click",()=>{
+    let title=document.getElementById("title");
+    title.style.color="blue";
+});
+let nameinput=document.getElementById("nameInput");
+nameinput.addEventListener("input",()=>{
+    let title=document.getElementById("title");
+    title.textContent="Hello, "+nameinput.value+"!";
+}); 
+
+let addpara=document.getElementById("addBtn");
+addpara.addEventListener("click",()=>{
+    let paragraphContainer=document.getElementById("paragraphContainer");
+    paragraphContainer.innerHTML="<p>this is the para that i have newly added</p>";
+    
+});
+let removetxt=document.getElementById("removeBtn");
+removetxt.addEventListener("click",()=>{
+    let paragraphContainer=document.getElementById("paragraphContainer");
+    paragraphContainer.innerHTML="";
+});
+
